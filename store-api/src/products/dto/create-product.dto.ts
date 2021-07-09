1491721f0bcf1be30af0c1e7a1c891dc88b06627
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-07-09 08:15:29 -0300

    validando DTO de produto

--- store-api/src/products/dto/create-product.dto.ts
+++ store-api/src/products/dto/create-product.dto.ts
@@ -1,11 +1,28 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
 export class CreateProductDto {
+  @MaxLength(255)
+  @IsNotEmpty()
+  @IsString()
   name: string;
 
+  @IsNotEmpty()
+  @IsString()
   description: string;
 
+  @MaxLength(255)
+  @IsNotEmpty()
+  @IsString()
   image_url: string;
 
-  slug: string;
-
+  @IsPositive()
+  @IsNumber()
+  @IsNotEmpty()
   price: number;
 }

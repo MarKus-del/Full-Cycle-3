3f85d3e62e7a37a74f0a18a5892b9232921b0909
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-07-09 10:07:47 -0300

    criando navbar e dark theme

--- store-frontend/src/pages/_app.tsx
+++ store-frontend/src/pages/_app.tsx
@@ -1,6 +1,27 @@
+import { Container, CssBaseline, MuiThemeProvider, Box } from '@material-ui/core'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
+import Navbar from '../components/NavBar'
+import theme from './theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    jssStyles?.parentElement?.removeChild(jssStyles);
+  }, []);
+
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Navbar />
+      <Container>
+        <Box marginTop={1}>
+          <Component {...pageProps} />
+        </Box>
+      </Container>
+    </MuiThemeProvider>
+  )
 }
 export default MyApp

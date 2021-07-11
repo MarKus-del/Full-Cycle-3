880443d6e692ee9b4ba0e4d0fa59ee8a87ebe8a5
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-07-10 23:05:44 -0300

    adicionando comando sql para teste

--- codebank/db.sql
+++ codebank/db.sql
@@ -20,3 +20,6 @@ CREATE TABLE transactions (
 	created_at timestamp not null,
   PRIMARY KEY (id)
 );
+
+INSERT INTO credit_cards(id, name, number, expiration_month, expiration_year, CVV, balance, balance_limit)
+VALUES('1a1c5e5b-f5a3-4bc4-ae83-067f269aa472', 'fulano', '1111111111111103', 7, 2021, '123', 0, 1000000000);
